dd5bfcfc1da37dfc5e49f77652dfbc47b58f6b07
Martijn van Maurik <martijn@vmaurik.nl>
2014-08-29 09:51:48 +0200

    Oops forgotten!

--- before.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir build && cd build && cmake ../ && make && cd ..
-mkdir -p .openssl/lib
-cd .openssl && ln -s ../include ./
-cd ..
-cp build/crypto/libcrypto.a build/ssl/libssl.a .openssl/lib
-touch .openssl/include/ssl.h
