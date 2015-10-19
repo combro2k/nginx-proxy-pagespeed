7b6a74c6c3b9255bee224b14d0da26c4dfb0016c
Martijn van Maurik <martijn@vmaurik.nl>
2015-10-19 15:24:14 +0200

    Use 2048 instead of 4096 Fix the run script Fix the nginx template

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti --rm --name nginx-proxy-pagespeed combro2k/nginx-proxy-pagespeed:boringssl ${@}
+docker run -ti --rm --name nginx-proxy-pagespeed -v /var/run/docker.sock:/tmp/docker.sock:ro combro2k/nginx-proxy-pagespeed:boringssl ${@}
