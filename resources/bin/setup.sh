a6511034c50f05731a195fbff01323f9d561f5e3
Martijn van Maurik <martijn@vmaurik.nl>
2016-02-10 10:29:34 +0100

    Update sources/versions

--- resources/bin/setup.sh
+++ resources/bin/setup.sh
@@ -6,10 +6,10 @@ declare -A NGX_MODULES
 export DEBIAN_FRONTEND="noninteractive"
 
 # Versions
-export NGINX_VERSION="1.9.9"
-export NPS_VERSION="1.9.32.11"
-export DOCKER_GEN="0.4.3"
-export OPENSSL_VERSION="1.0.2e"
+export NGINX_VERSION="1.9.11"
+export NPS_VERSION="1.10.33.4"
+export DOCKER_GEN="0.5.0"
+export OPENSSL_VERSION="1.0.2f"
 
 # Packages
 export PACKAGES=(
