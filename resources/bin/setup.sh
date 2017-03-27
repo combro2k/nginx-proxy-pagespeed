d9f31a27b5ae5ff320a2e4fc0d7645821cf7b029
Combro2k <martijn@vmaurik.nl>
2017-03-27 12:20:06 +0200

    Update setup.sh

--- resources/bin/setup.sh
+++ resources/bin/setup.sh
@@ -6,10 +6,10 @@ declare -A NGX_MODULES
 export DEBIAN_FRONTEND="noninteractive"
 
 # Versions
-export NGINX_VERSION="1.11.5"
-export NPS_VERSION="1.11.33.4"
+export NGINX_VERSION="1.11.12"
+export NPS_VERSION="1.12.34.2"
 export DOCKER_GEN="0.7.3"
-export OPENSSL_VERSION="1.1.0b"
+export OPENSSL_VERSION="1.1.0e"
 
 # Packages
 export PACKAGES=(
